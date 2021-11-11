4305fef82ecb4660848107f862efef3d8e9cf12e
Shashi Suman <shashisuman17@aol.com>
2021-11-11 11:26:08 -0500

    Update initialize_etl.py

--- initialize_etl.py
+++ initialize_etl.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup as BS
 import sys
 from pytz import timezone
 import get_stock_data_etl as ETL
-import AWSkeys
+import config
 
 stock_data_tabular = {}
 base_search_query = 'https://www.google.com/search?q='
 time_ = time.strftime("%Y%m%d, %H%M%S", time.localtime()).split(',')[1].strip()
-s3client = AWS_S3('StocksBucket',access_key=AWSkeys.access_key, secret_key=AWSkeys.secret_key)
+s3client = AWS_S3('StocksBucket',access_key=config.access_key, secret_key=config.secret_key)
 s3client.download_from_bucket('stocks.txt','stock_list.txt')
 with open('stock_list.txt, 'r') as file:
     stock = f.readline().strip('\n')
